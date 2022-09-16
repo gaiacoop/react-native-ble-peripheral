cd49c0ac196c157130be1115d637e77057ad35ca
heroims <heroims@163.com>
2022-09-16 17:12:36 +0800

    update wrong

--- android/src/main/java/com/himelbrand/ble/peripheral/RNBLEModule.java
+++ android/src/main/java/com/himelbrand/ble/peripheral/RNBLEModule.java
@@ -311,5 +311,16 @@ public class RNBLEModule extends ReactContextBaseJavaModule{
         promise.resolve(this.advertising);
     }
 
+    @ReactMethod
+    public void addListener(String eventName) {
+        // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+    @ReactMethod
+    public void removeListeners(Integer count) {
+        // Keep: Required for RN built in Event Emitter Calls.
+    }
+
+
 
 }
